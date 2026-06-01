d1166df5b2ea5a4b60b6ef6666b4286549b18408
ziaulhoque <ziaulhoquejp@gmail.com>
2026-06-01 22:20:22 +0900

    Add Google Search Console verification

--- app/layout.tsx
+++ app/layout.tsx
@@ -23,6 +23,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head>
+      <meta name="google-site-verification" content="ZOe050wotNJeisPcWW49d6iVWB-cBoqAGpifN8WEnac" />  
         <link rel="manifest" href="/manifest.json" />
 <meta name="theme-color" content="#C42020" />
 <meta name="apple-mobile-web-app-capable" content="yes" />

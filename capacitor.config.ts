5b70ce0459c6c1f504dbda9f1b5da03973857e57
ziaulhoque <ziaulhoquejp@gmail.com>
2026-07-16 10:58:42 +0900

    Fix iOS capacitor config for iPad

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,13 +1,17 @@
 import type { CapacitorConfig } from '@capacitor/cli';
-
 const config: CapacitorConfig = {
   appId: 'app.japanlifeguide.www',
   appName: 'Japan Life Guide',
   webDir: 'public',
   server: {
     url: 'https://japanlifeguide.app',
-    cleartext: true
-  }
+    cleartext: true,
+    allowNavigation: ['japanlifeguide.app', '*.japanlifeguide.app'],
+  },
+  ios: {
+    allowsLinkPreview: false,
+    scrollEnabled: true,
+    contentInset: 'always',
+  },
 };
-
 export default config;
